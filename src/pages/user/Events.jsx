5ee3e1ecd91ef46be2d9329bfc758e5e3e07c5cd
ChetanPW007 <chetanpw75@gmail.com>
2025-12-15 21:56:50 +0530

    Update Events.jsx

--- src/pages/user/Events.jsx
+++ src/pages/user/Events.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./events.css";
 import EventCard from "../../components/EventCard";
 import Loading from "../../components/Loading";
 
